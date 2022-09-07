21d59562bc33279962dc852d895dcfbd14f68b3c
Kin Chang <ckwojai@gmail.com>
2022-09-07 23:48:35 +0000

    hw2 code

--- hw2/readLibrary.cpp
+++ hw2/readLibrary.cpp
@@ -1,4 +1,20 @@
-bool readUnix() {
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+bool readReverse() {
+  char buf[100];
+  int f_read = open("start.txt", O_RDONLY);
+  int f_write = open("end.txt", O_WRONLY);
+  lseek(f_read, -9, SEEK_END);
+  read(f_read, buf, 7);
+  printf("%s", buf);
+  return true;
 }
-bool readC() {
+int main() {
+  readReverse();
+  return 0;
 }
+
