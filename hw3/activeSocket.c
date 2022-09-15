16b834dcd6b2b1b0706eb623b32f9489e387caff
Kin Chang <kinchang@kins-mbp.lan>
2022-09-15 14:48:35 -0700

    use addrinfo instead of deprecated gethostbyname and getservbyname

--- hw3/activeSocket.c
+++ hw3/activeSocket.c
@@ -1,6 +1,8 @@
 #include "activeSocket.h"
 #include "errexit.h"
 
+#include <sys/socket.h> //socket()
+#include <sys/types.h> // addrino
 #include <netinet/in.h> // sockaddr_in struct
 #include <netdb.h> // getprotobyname(), getservbyname()
 #include <string.h> // strcmp()
@@ -14,36 +16,52 @@ int connectsock(const char* host, const char* service, const char* transport) {
 
     struct protoent* ppe; // pointer to protocol info entry
     // Get protocol number from protocol name
-    if ((ppe = getprotobyname(transport)) == 0) {
-        errexit("can't get \"%s\" protocol entry\n", transport);
-    }
+    /* if ((ppe = getprotobyname(transport)) == 0) { */
+    /*     errexit("can't get \"%s\" protocol entry\n", transport); */
+    /* } */
 
     if (strcmp(transport, "udp") == 0)
         type = SOCK_DGRAM;
     else
         type = SOCK_STREAM;
 
-    // for connection
-    struct sockaddr_in sin;
-    // pointer to service info entry
-    struct servent* pse;
-    if ((pse = getservbyname(service, transport))) { // example of service: "daytime"
-        sin.sin_port = pse->s_port; // service port number, different than protocol number
-    } else {
-        errexit("can't get \"%s\" service entry\n", service);
+    struct addrinfo* addrinfo;
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof hints);
+    hints.ai_socktype = type;
+    hints.ai_family = PF_UNSPEC;
+    int status = getaddrinfo(host, service, &hints, &addrinfo);
+    if (status != 0) {
+        errexit("can't getaddrinfo: %s \n", gai_strerror(status));
     }
+    /* - service determines the port */
+    /* - hints.socktype determines the protocol */
+    /* - host, together with the above get combined to sockaddr in .ai_addr that can be used to create socket directly */
 
-    // convert hostname to hostaddr and copy to sin
-    if (inet_pton(AF_INET, host, &sin.sin_addr) <= 0) {
-        errexit("can't get \"%s\" host entry. %s\n", host, strerror(errno));
-    } // example of host: "google.com"
+    /* // for connection */
+    /* struct sockaddr_in sin; */
+    /* // pointer to service info entry */
+    /* struct servent* pse; */
+    /* if ((pse = getservbyname(service, transport))) { // example of service: "daytime" */
+    /*     sin.sin_port = pse->s_port; // service port number, different than protocol number */
+    /* } else { */
+    /*     errexit("can't get \"%s\" service entry\n", service); */
+    /* } */
+
+    /* // convert hostname to hostaddr and copy to sin */
+    /* if (inet_pton(AF_INET, host, &sin.sin_addr) <= 0) { */
+    /*     errexit("can't get \"%s\" host entry. %s\n", host, strerror(errno)); */
+    /* } // example of host: "google.com" */
 
 
     // Create socket
-    sockfd = socket(ADDR_FAM, type, ppe->p_proto);
+    sockfd = socket(ADDR_FAM, type, addrinfo->ai_protocol);
     // Create connection
-    if (connect(sockfd, (struct sockaddr*) &sin, sizeof(sin)) < 0)
+    if (connect(sockfd, addrinfo->ai_addr, addrinfo->ai_addrlen) < 0)
         errexit("can't connect to &s.%s: %s\n", host, service, strerror(errno)); // strerror converts errno to readable string
 
+    // Free addrinfo
+    freeaddrinfo(addrinfo);
+    freeaddrinfo(&hints);
     return sockfd; // consumer of this function need to close this socket using: close(sockfd)
 }
