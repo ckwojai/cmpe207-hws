9bfaac5a7307abf160d775e1d2020fdb935b3815
Kin Chang <kinchang@Kins-MacBook-Pro.local>
2022-09-22 14:48:34 -0700

    add print out for server

--- hw4/server.c
+++ hw4/server.c
@@ -11,6 +11,27 @@
 #define LINELEN 128
 #define UNIXEPOCH 2208988800 /* UNIX epoch, in UCT secs */
 
+char *get_ip_str(const struct sockaddr *sa, char *s, size_t maxlen)
+{
+    switch(sa->sa_family) {
+        case AF_INET:
+            inet_ntop(AF_INET, &(((struct sockaddr_in *)sa)->sin_addr),
+                    s, maxlen);
+            break;
+
+        case AF_INET6:
+            inet_ntop(AF_INET6, &(((struct sockaddr_in6 *)sa)->sin6_addr),
+                    s, maxlen);
+            break;
+
+        default:
+            strncpy(s, "Unknown AF", maxlen);
+            return NULL;
+    }
+
+    return s;
+}
+
 int main() {
    const char* service = "time";
    char buf[1];
@@ -23,9 +44,16 @@ int main() {
       if (recvfrom(sockfd, buf, sizeof(buf), 0, (struct sockaddr*)&fsin, &alen) < 0) {
          errexit("recvfrom: %s\n", strerror(errno));
       }
-      printf("Receive connection. Sending time back...");
+
+      // Convert fsin to address in string
+      char clientAddr[100];
+      (void) get_ip_str((struct sockaddr*) &fsin, clientAddr, 100);
+
       (void) time(&now);
+      printf("Receive connection from %s. Sending back time: %s...", clientAddr, ctime(&now));
       now = htonl((u_long)(now + UNIXEPOCH));
+
       (void) sendto(sockfd, (char *)&now, sizeof(now), 0, (struct sockaddr*)&fsin, sizeof(fsin));
+      printf("...SUCCESS\n");
    }
 }
