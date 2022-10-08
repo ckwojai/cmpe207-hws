5cc076c76b988ec240ce4eb1e503a82f6029b41a
Kin Chang <kinchang@kins-mbp.lan>
2022-10-07 21:58:26 -0700

    client establish a connection in for loop

--- hw6/fileclient.c
+++ hw6/fileclient.c
@@ -11,23 +11,23 @@
 int main() {
    const char* host = "localhost";
    const char* service = "ftp";
-   int sockfd = connectTCP(host, service);
-   printf("Connected to %s service on %s :\n", service, host);
    char buf[BUFSIZE];
 
    for (int i=0; i < 5; i++) {
+      // establish a new connection
+      int sockfd = connectTCP(host, service);
       char filename[20];
       snprintf(filename, 20, "%d_client.txt", i+1);
       int fd = open(filename, O_RDONLY);
       int filesize = lseek(fd, 0, SEEK_END);
       lseek(fd, 0, SEEK_SET);
       read(fd, &buf, filesize);
-      printf("Sending %s to server.", filename);
+      printf("Sending %s to server......", filename);
       write(sockfd, &buf, filesize);
+      printf("Done\n");
       // reset buf to clear junk
       memset(buf, '\0', strlen(buf));
+      close(sockfd);
    }
-
-   close(sockfd);
    return 0;
 }
