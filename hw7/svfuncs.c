582728c9a1521ea2745951b6a8a5c4b5ca0e33e7
Kin Chang <kinchang@kins-mbp.lan>
2022-10-26 08:51:38 -0700

    implement udp funcs add print statement for svfuncs

--- hw7/svfuncs.c
+++ hw7/svfuncs.c
@@ -11,6 +11,8 @@
 #include <time.h> // time()
 
 #define	BUFSIZ	4096		/* max read buffer size	*/
+#define	LINELEN		72
+#define	UNIXEPOCH	2208988800	/* UNIX epoch, in UCT secs	*/
 
 extern	int	errno;
 
@@ -32,19 +34,21 @@ void printFinish(char* transport, char* service) {
 int
 TCPechod(int fd)
 {
+	char* transport = "tcp";
+	char* service = "echo";
 	char	buf[BUFSIZ];
 	int	cc;
-
+	printReceive(transport, service);
 	while (cc = read(fd, buf, sizeof buf)) {
 		if (cc < 0)
 			errexit("echo read: %s\n", strerror(errno));
 		if (write(fd, buf, cc) < 0);
 			errexit("echo write: %s\n", strerror(errno));
 	}
+	printFinish(transport, service);
 	return 0;
 }
 
-#define	LINELEN		72
 
 /*------------------------------------------------------------------------
  * TCPchargend - do TCP CHARGEN on the given socket
@@ -53,11 +57,14 @@ TCPechod(int fd)
 int
 TCPchargend(int fd)
 {
-	char	c, buf[LINELEN+2];	/* print LINELEN chars + \r\n */
+	char* transport = "tcp";
+	char* service = "chargen";
+	char c, buf[LINELEN+2];	/* print LINELEN chars + \r\n */
 
 	c = ' ';
 	buf[LINELEN] = '\r';
 	buf[LINELEN+1] = '\n';
+	printReceive(transport, service);
 	while (1) {
 		int	i;
 
@@ -69,6 +76,7 @@ TCPchargend(int fd)
 		if (write(fd, buf, LINELEN+2) < 0)
 			break;
 	}
+	printFinish(transport, service);
 	return 0;
 }
 
@@ -79,16 +87,20 @@ TCPchargend(int fd)
 int
 TCPdaytimed(int fd)
 {
+	char* transport = "tcp";
+	char* service = "daytime";
+	printReceive(transport, service);
+
 	char	buf[LINELEN], *ctime();
 	time_t	time(), now;
 
 	(void) time(&now);
 	sprintf(buf, "%s", ctime(&now));
 	(void) write(fd, buf, strlen(buf));
+	printFinish(transport, service);
 	return 0;
 }
 
-#define	UNIXEPOCH	2208988800	/* UNIX epoch, in UCT secs	*/
 
 /*------------------------------------------------------------------------
  * TCPtimed - do TCP TIME protocol
@@ -97,10 +109,15 @@ TCPdaytimed(int fd)
 int
 TCPtimed(int fd)
 {
+	char* transport = "tcp";
+	char* service = "time";
+
+	printReceive(transport, service);
 	time_t	now;
 	(void) time(&now);
 	now = htonl((u_long)(now + UNIXEPOCH));
 	(void) write(fd, (char *)&now, sizeof(now));
+	printFinish(transport, service);
 	return 0;
 }
 
@@ -110,6 +127,19 @@ TCPtimed(int fd)
  */
 
 int UDPechod(int fd) {
+	char* transport = "udp";
+	char* service = "echo";
+
+	char buf[BUFSIZ];
+
+	struct sockaddr_in fsin;
+	socklen_t alen;
+	printReceive(transport, service);
+	if (recvfrom(fd, buf, sizeof(buf), 0, (struct sockaddr*)&fsin, &alen) < 0) {
+         errexit("recvfrom: %s\n", strerror(errno));
+	}
+	(void) sendto(fd, (char *)&buf, sizeof(buf), 0, (struct sockaddr*)&fsin, sizeof(fsin));
+	printFinish(transport, service);
 	return 0;
 }
 
@@ -118,6 +148,30 @@ int UDPechod(int fd) {
  *------------------------------------------------------------------------
  */
 int UDPchargend(int fd) {
+	char* transport = "udp";
+	char* service = "chargen";
+	char c, buf[LINELEN+2];	/* print LINELEN chars + \r\n */
+
+	c = ' ';
+	buf[LINELEN] = '\r';
+	buf[LINELEN+1] = '\n';
+	int	i;
+
+	for (i=0; i<LINELEN; ++i) {
+		buf[i] = c++;
+		if (c > '~')
+			c = ' ';
+	}
+
+	char request[1];
+	struct sockaddr_in fsin;
+	socklen_t alen;
+	printReceive(transport, service);
+	if (recvfrom(fd, request, sizeof(request), 0, (struct sockaddr*)&fsin, &alen) < 0) {
+         errexit("recvfrom: %s\n", strerror(errno));
+	}
+	(void) sendto(fd, (char *)&buf, sizeof(buf), 0, (struct sockaddr*)&fsin, sizeof(fsin));
+	printFinish(transport, service);
 	return 0;
 }
 
@@ -126,6 +180,24 @@ int UDPchargend(int fd) {
  *------------------------------------------------------------------------
  */
 int UDPdaytimed(int fd) {
+	char* transport = "udp";
+	char* service = "daytime";
+
+	char buf[LINELEN], *ctime();
+	time_t	time(), now;
+	char request[1];
+
+	struct sockaddr_in fsin;
+	socklen_t alen;
+	if (recvfrom(fd, request, sizeof(request), 0, (struct sockaddr*)&fsin, &alen) < 0) {
+         errexit("recvfrom: %s\n", strerror(errno));
+	}
+	printReceive(transport, service);
+
+	(void) time(&now);
+	sprintf(buf, "%s", ctime(&now));
+	(void) sendto(fd, (char *)&buf, sizeof(buf), 0, (struct sockaddr*)&fsin, sizeof(fsin));
+	printFinish(transport, service);
 	return 0;
 }
 /*------------------------------------------------------------------------
@@ -141,7 +213,7 @@ int UDPtimed(int fd) {
 	socklen_t alen;
 	if (recvfrom(fd, buf, sizeof(buf), 0, (struct sockaddr*)&fsin, &alen) < 0) {
          errexit("recvfrom: %s\n", strerror(errno));
-      }
+	}
 	printReceive(transport, service);
 	time_t now;
 	(void) time(&now);
