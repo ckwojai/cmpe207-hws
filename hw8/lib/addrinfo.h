eaf1a62b469232c4aac30b3784d83b05f33ea962
Kin Chang <ckwojai@gmail.com>
2022-11-03 12:37:53 -0700

    strip to essentials for hw8

--- hw8/lib/addrinfo.h
+++ hw8/lib/addrinfo.h
@@ -7,16 +7,16 @@
  * system's header.
  */
 
-/* struct addrinfo { */
-/*   int		ai_flags;			/\* AI_PASSIVE, AI_CANONNAME *\/ */
-/*   int		ai_family;			/\* PF_xxx *\/ */
-/*   int		ai_socktype;		/\* SOCK_xxx *\/ */
-/*   int		ai_protocol;		/\* IPPROTO_xxx for IPv4 and IPv6 *\/ */
-/*   size_t	ai_addrlen;			/\* length of ai_addr *\/ */
-/*   char		*ai_canonname;		/\* canonical name for host *\/ */
-/*   struct sockaddr	*ai_addr;	/\* binary address *\/ */
-/*   struct addrinfo	*ai_next;	/\* next structure in linked list *\/ */
-/* }; */
+struct addrinfo {
+  int		ai_flags;			/* AI_PASSIVE, AI_CANONNAME */
+  int		ai_family;			/* PF_xxx */
+  int		ai_socktype;		/* SOCK_xxx */
+  int		ai_protocol;		/* IPPROTO_xxx for IPv4 and IPv6 */
+  size_t	ai_addrlen;			/* length of ai_addr */
+  char		*ai_canonname;		/* canonical name for host */
+  struct sockaddr	*ai_addr;	/* binary address */
+  struct addrinfo	*ai_next;	/* next structure in linked list */
+};
 
 			/* following for getaddrinfo() */
 #define	AI_PASSIVE		 1	/* socket is intended for bind() + listen() */
