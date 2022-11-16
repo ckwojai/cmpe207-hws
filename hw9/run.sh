b7da9721436e29f205528427c3900d206608fa58
Kin Chang <kinchang@kins-mbp.lan>
2022-11-15 23:25:42 -0800

    no need arm configure

--- hw9/run.sh
+++ hw9/run.sh
@@ -1,6 +1,6 @@
 ##!/bin/sh
 source clean.sh
-./configure --build=aarch64-unknown-linux-gnu
+./configure
 cd lib && make clean && make
 cd ../libfree && make clean && make
 cd ../ping && make clean && make
